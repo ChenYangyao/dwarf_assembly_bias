ffd2aefd009cd9e0abd81795500e5be3101fe617
Yangyao Chen <yangyaochen.astro@foxmail.com>
2025-10-18 15:59:33 +0800

    remove PYTHONPATH dependency

--- tests/dwarf_assembly_bias/test_env.py
+++ tests/dwarf_assembly_bias/test_env.py
@@ -1,7 +1,7 @@
 import os, dwarf_assembly_bias
 
 def test_env():
-    print('Python PATH:', os.environ['PYTHONPATH'])    
+    print('Python PATH:', os.environ.get('PYTHONPATH', ''))    
     
 def test_version():
     print(dwarf_assembly_bias.version)
